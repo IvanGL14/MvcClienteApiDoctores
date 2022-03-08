bf4c7bd6343d927eaae6d673499b21d6134f53e5
IvanGL14 <vanchi127@gmail.com>
2022-03-08 12:32:53 +0100

    Add project files.

---
+++ MvcClienteApiDoctores/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcClienteApiDoctores.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
